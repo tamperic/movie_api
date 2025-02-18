24f8d097d20a4dae94ae4d77f22295560b27373f
tamperic <tamaraperic.work@gmail.com>
2025-02-18 18:58:44 +0100

    Define schema

--- models.js
+++ models.js
@@ -1,3 +1,28 @@
 const mongoose = require('mongoose');
+
+let movieSchema = mongoose.Schema({
+    Title: {type: String, required: true},
+    Description: {type: String, required: true},
+    Genre: {
+        Name: String, 
+        Description: String
+    },
+    Director: {
+        Name: String,
+        Bio: String
+    },
+    Actors: [String], 
+    ImagePath: String,
+    Featured: boolean
+});
+
+let userSchema = mongoose.Schema({
+    Username: {type: String, required: true},
+    Password: {type: String, requiire: true},
+    Email: {type: String, required: true},
+    Birthday: Date,
+    FavoriteMovies: [{type: mongoose.Schema.Types.ObjectId, ref: 'Movie'}]
+});
+
 module.export.Movie = Movie;
 module.export.User = User;
